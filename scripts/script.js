8bb033ce610152254534e4d056606370f01b5c72
Puneet Singh <199150760+ps-mullhi@users.noreply.github.com>
2025-02-18 11:26:50 -0800

    Make adjustable grid with mouse hover effect.
    User can enter in how big they want the etch a sketch to be. It also
    fills in based on which grids the mouse hovers over.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,47 @@
-document.querySelector('body').textContent += " Project";
+function generateAndInsertDivs(numDivs) {
+    const container = document.querySelector("#container");
+
+    for(let i = 0; i < numDivs; i++){
+        let newDiv = document.createElement("div");
+        let divId = "div" + (i+1);
+        newDiv.classList.add("gridDiv")
+        newDiv.setAttribute("id", divId);
+        newDiv.addEventListener("mouseover", () => {
+            newDiv.classList.add("gridHover");
+        });
+
+        container.appendChild(newDiv);
+    }
+}
+
+function main() {
+    generateAndInsertDivs(20);
+}
+
+const gridSizeButton = document.querySelector("#userInput");
+gridSizeButton.addEventListener("click", () => {
+    let gridSizeInput;
+    do {
+        gridSizeInput = prompt("Enter in number of squares per side of grid (max 100):");
+        parseInt(gridSizeInput);
+        if(isNaN(gridSizeInput)) {
+            alert("NOT A WHOLE NUMBER");
+        }
+    }  while (isNaN(gridSizeInput));
+
+    adjustGridSize(gridSizeInput);
+});
+
+function adjustGridSize(numSquaresPerSide) {
+    const container = document.querySelector("#container");
+    container.innerHTML = "";
+
+    generateAndInsertDivs(numSquaresPerSide * numSquaresPerSide);
+
+    let containerWidth = numSquaresPerSide * 10 + (numSquaresPerSide * 2);
+    container.style.width = containerWidth + "px";
+
+}
+
+
+main();
